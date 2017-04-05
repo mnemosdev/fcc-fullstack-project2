98ff99ebd6a9d4e22419b2c0fd9ffd60b288aece
Lorenzo Pieri <lorenzopieri.lavoro@gmail.com>
2017-04-05 10:48:02 +0000

    github and passport authentication

--- app/config/passport-google.js
+++ app/config/passport-google.js
@@ -18,51 +18,51 @@ module.exports = function(passport){
       done(null, obj);
     });
     
-   passport.use(new GoogleStrategy({
-     
-      clientID: configAuth.googleAuth.clientID,
-      clientSecret: configAuth.googleAuth.clientSecret,
-      callbackURL: configAuth.googleAuth.callbackURL
-      
+   // =========================================================================
+    // GOOGLE ==================================================================
+    // =========================================================================
+    passport.use(new GoogleStrategy({
+
+        clientID        : configAuth.googleAuth.clientID,
+        clientSecret    : configAuth.googleAuth.clientSecret,
+        callbackURL     : configAuth.googleAuth.callbackURL,
+
     },
-    function(accessToken, refreshToken, profile, done) {
-      
-      process.nextTick(function () {
-        
-        // Find the user if exists
-        User.findOne({ "google.id" : profile.id }, function(err, user){
-          if (err){
-            // error
-            return done(err);
-          }
-          
-          if (user){
-            // user found
-            return done(null, user);
-          }
-          
-          else {
-            // if the user doesn't exist, create a new one
-            var newUser = new User();
-            
-            // set all the infomations about the user
-            newUser.google.id = profile.id;
-            newUser.google.token = accessToken;
-            newUser.google.displayName = profile.displayName;
-            
-            // save the user
-            newUser.save(function(err){
-              if (err){
-                throw err;
-              }
-              
-              currentUser.displayName = user.displayName;
-              currentUser.id = user.id;
-              return done(null, newUser);
-            })
-          }
-        })
-      });
-    }
-  ));
+    function(token, refreshToken, profile, done) {
+
+        // make the code asynchronous
+        // User.findOne won't fire until we have all our data back from Google
+        process.nextTick(function() {
+
+            // try to find the user based on their google id
+            User.findOne({ 'google.id' : profile.id }, function(err, user) {
+                if (err)
+                    return done(err);
+
+                if (user) {
+
+                    // if a user is found, log them in
+                    return done(null, user);
+                } else {
+                    // if the user isnt in our database, create a new user
+                    var newUser          = new User();
+
+                    // set all of the relevant information
+                    newUser.google.id    = profile.id;
+                    newUser.google.token = token;
+                    newUser.google.name  = profile.displayName;
+                    newUser.google.email = profile.emails[0].value; // pull the first email
+
+                    // save the user
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+
+    }));
+
 }
